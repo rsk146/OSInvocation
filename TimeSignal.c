53bf3c5d36f040f4ff08147e9dcf1b1d0619a35a
Krishna Prajapati <kjp189@scarletmail.rutgers.edu>
2020-09-16 22:02:52 -0400

    Sketched out prototype of function

--- TimeSignal.c
+++ TimeSignal.c
@@ -1,6 +1,25 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <signal.h>
+#include <sys/time.h>
+
+int counter = 0;
+int maxIterations = 2;
+int zero = 0;
+
+void signalHandle(int signum){
+  counter++;
+}
 
 int main(int argc, char** argv){
-    
+
+  signal(SIGFPE, signalHandle);
+  struct timeval start, end;
+
+  int i;
+  gettimeofday(&start, NULL);
+  while(counter < maxIterations) i = 10/zero;
+  gettimeofday(&end, NULL);
+
+  printf("Start time: %ld\nEnd time: %ld\n", start.tv_usec, end.tv_usec);
 }
