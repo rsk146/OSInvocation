d3317c484c3d4f1a7604f889c64d5a06dca3626d
Krishna Prajapati <kjp189@scarletmail.rutgers.edu>
2020-09-16 23:09:17 -0400

    Finished basic of timeSignal

--- TimeSignal.c
+++ TimeSignal.c
@@ -3,23 +3,35 @@
 #include <signal.h>
 #include <sys/time.h>
 
-int counter = 0;
-int maxIterations = 2;
 int zero = 0;
 
-void signalHandle(int signum){
+int counter = 0;
+int maxIters = 100000;
+
+struct timeval* start;
+struct timeval* end;
+
+void handle_signalFPE(int signum){
   counter++;
+  if(counter >= maxIters){
+    gettimeofday(end, NULL);
+    
+    //printf("Start time: %ld\nEnd time: %ld\n", start->tv_usec, end->tv_usec);
+    printf("Exceptions Occurred: %d\n", maxIters);
+    printf("Total Elapsed Time: %ld microseconds\n", end->tv_usec - start->tv_usec);
+    printf("Average Time Per SysCall: %f microseconds\n", (end->tv_usec - start->tv_usec)/100000.);
+
+    free(start); free(end);
+    exit(0);
+  }
+
 }
 
 int main(int argc, char** argv){
-
-  signal(SIGFPE, signalHandle);
-  struct timeval start, end;
-
-  int i;
-  gettimeofday(&start, NULL);
-  while(counter < maxIterations) i = 10/zero;
-  gettimeofday(&end, NULL);
-
-  printf("Start time: %ld\nEnd time: %ld\n", start.tv_usec, end.tv_usec);
+  start = malloc(sizeof(struct timeval));
+  end = malloc(sizeof(struct timeval));
+  signal(SIGFPE, handle_signalFPE);
+  
+  gettimeofday(start, NULL);
+  int i = 1/zero;
 }
